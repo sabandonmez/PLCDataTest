912d1358461cee39739847cdbb3a187fdfb09c47
Şaban Dönmez <eren.donmez.11@gmail.com>
2024-02-29 14:14:26 +0300

    modbus ile bağlantı kuruldu.

---
+++ Entities/DTOs/ModbusConnectDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class ModbusConnectDto
+    {
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+        public byte SlaveId { get; set; }
+        public ushort DataAddress { get; set; } // 0 - 65535 arasında bir değer
+        public ushort DataSize { get; set; } // Veri boyutu
+        public string WriteValue { get; set; } // Yazma işlemi için kullanılacak değer
+        public string ReadValue { get; set; } // Okuma işlemi sonucunda döndürülecek değer
+    }
+}
--- PLCDataTestApp/Controllers/ModbusController.cs
+++ PLCDataTestApp/Controllers/ModbusController.cs
@@ -1,3 +1,5 @@
+using EasyModbus;
+using Entities.DTOs;
 using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,6 +7,7 @@ namespace PLCDataTestApp.Controllers
 {
     public class ModbusController:Controller
     {
+
     public IActionResult Index()
     {
        var model = new ModbusOperationModel
@@ -14,11 +17,68 @@ namespace PLCDataTestApp.Controllers
             return View(model);
     }
 
-    [HttpPost]
-    public IActionResult Index(ModbusOperationModel model)
-    {
-        return View(model); 
-    }
+        [HttpPost]
+        public IActionResult Index(string ipAddress, int port, int slaveId)
+        {
+            try
+            {
+                ModbusClient modbusClient = new ModbusClient(ipAddress, port)
+                {
+                    UnitIdentifier = (byte)slaveId
+                };
+
+                modbusClient.Connect();
+                modbusClient.Disconnect(); 
+
+                return Json(true); 
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult ReadData(ModbusConnectDto model)
+        {
+            try
+            {
+                    var modbusClient = new ModbusClient(model.IpAddress, model.Port);
+                    modbusClient.UnitIdentifier = model.SlaveId;
+                    modbusClient.Connect();
+                    int[] readData = modbusClient.ReadHoldingRegisters(model.DataAddress, model.DataSize);
+                    modbusClient.Disconnect();
+                    return Json(new { success = true, data = readData }); 
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult WriteData(ModbusConnectDto model)
+        {
+            try
+            {
+                    var modbusClient = new ModbusClient(model.IpAddress, model.Port);
+                    modbusClient.UnitIdentifier = model.SlaveId; 
+                    modbusClient.Connect();
+                    int writeValue = int.Parse(model.WriteValue);
+                    modbusClient.WriteMultipleRegisters(model.DataAddress, new int[] { writeValue });
+                    modbusClient.Disconnect();
+                    return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+
+
     }
 }
 
